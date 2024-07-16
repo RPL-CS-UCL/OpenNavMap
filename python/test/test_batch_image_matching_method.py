332bfa96253b9c703bdc5d3de9948c7194e2e2a4
gogojjh <jiaojh1994@gmail.com>
2024-07-16 19:30:47 +0100

    add visualization of depth image in duster

--- python/test/test_batch_image_matching_method.py
+++ python/test/test_batch_image_matching_method.py
@@ -11,10 +11,11 @@ import argparse
 import matplotlib
 from pathlib import Path
 import numpy as np
+import matplotlib.pyplot as pl
+pl.ion()
 
 from pycpptools.python.utils_math.tools_eigen import compute_relative_dis, compute_relative_dis_TF, convert_vec_to_matrix
-from matching.utils import get_image_pairs_paths, to_numpy
-from matching import available_models
+from matching.utils import to_numpy
 
 from utils.utils_image_matching_method import *
 from image_graph import ImageGraphLoader
@@ -22,22 +23,6 @@ from image_graph import ImageGraphLoader
 # This is to be able to use matplotlib also without a GUI
 if not hasattr(sys, "ps1"):
 	matplotlib.use("Agg")
-
-def setup_args():
-	"""Setup command-line arguments."""
-	parser = argparse.ArgumentParser(description="Batch Image Matching Test",
-																		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument("--dataset_path", type=str, default="matterport3d", help="path to dataset_path")
-	parser.add_argument("--matcher", type=str, default="sift-lg", choices=available_models, help="choose your matcher")
-	parser.add_argument("--image_size", type=int, default=512, nargs="+",
-											help="Resizing shape for images (HxW). If a single int is passed, set the"
-											"smallest edge of all images to this value, while keeping aspect ratio")
-	parser.add_argument("--n_kpts", type=int, default=2048, help="max num keypoints")
-	parser.add_argument("--device", type=str, default="cuda", choices=["cpu", "cuda"])
-	parser.add_argument("--no_viz", action="store_true", help="pass --no_viz to avoid saving visualizations")
-	parser.add_argument("--sample_map", type=int, default=1, help="sample of map")
-	parser.add_argument("--sample_obs", type=int, default=1, help="sample of observation")
-	return parser.parse_args()
 
 def main(args):
 	"""Main function to run the image matching process."""
@@ -49,12 +34,11 @@ def main(args):
 
 	"""Load image data"""
 	path_map = os.path.join(args.dataset_path, 'map')
-	image_graph = ImageGraphLoader.load_data(path_map, image_size, normalized=False, num_sample=args.sample_map)
+	image_graph = ImageGraphLoader.load_data(path_map, image_size, args.depth_scale, normalized=False, num_sample=args.sample_map)
 	path_obs = os.path.join(args.dataset_path, 'obs')
-	image_obs = ImageGraphLoader.load_data(path_obs, image_size, normalized=False, num_sample=args.sample_obs)
+	image_obs = ImageGraphLoader.load_data(path_obs, image_size, args.depth_scale, normalized=False, num_sample=args.sample_obs)
 
-	rot_e = []
-	trans_e = []
+	rot_e, trans_e = [], []
 
 	"""Perform image matcher"""
 	start_time = time.time()
@@ -75,7 +59,7 @@ def main(args):
 		# Matching image pairs
 		try:
 			out_str = f"Paths: map_id ({map_id}), obs_id ({obs_id}). "
-			result = matcher(map_node.image, obs_node.image)
+			result = matcher(map_node.rgb_image, obs_node.rgb_image)
 			num_inliers, H, mkpts0, mkpts1 = (
 					result["num_inliers"],
 					result["H"],
@@ -86,7 +70,7 @@ def main(args):
 			
 			"""Save matching results"""
 			out_str += f"Found {num_inliers} inliers after RANSAC. "
-			viz_path = save_visualization(map_node.image, obs_node.image, mkpts0, mkpts1, log_dir, obs_id, n_viz=100)
+			viz_path = save_visualization(map_node.rgb_image, obs_node.rgb_image, mkpts0, mkpts1, log_dir, obs_id, n_viz=100)
 			out_str += f"Viz saved in {viz_path}. "
 			dict_path = save_output(result, None, None, args.matcher, args.n_kpts, image_size, log_dir, obs_id)
 			out_str += f"Output saved in {dict_path}"       
@@ -103,15 +87,38 @@ def main(args):
 			T_w_obs = convert_vec_to_matrix(obs_node.t_w_cam, obs_node.quat_w_cam, 'xyzw')
 			T_map_obs = np.linalg.inv(T_w_map) @ T_w_obs
 			print('Groundtruth Poses:\n', T_map_obs)
-
-			scene = matcher.scene	
 			# print('Estimated H:\n', H)
 			
-			# NOTE(gogojjh): definition of im_poses is given pair_viewer.py
+			scene = matcher.scene
+			
+			# DEBUG(gogojjh): analyze the generate rgb and depth image
+			rgb_image = to_numpy(map_node.rgb_image)
+			depth_image = to_numpy(map_node.depth_image)
+			save_input_images(rgb_image * 255, depth_image / args.depth_scale,
+												os.path.join(log_dir, f'map_gt_rgb_{map_id}.png'), 
+ 											  os.path.join(log_dir, f'map_gt_depth_{map_id}.png'))
+			rgb_image = scene.imgs[0]
+			depth_image = to_numpy(scene.get_depthmaps())[0]
+			save_duster_images(rgb_image * 255, depth_image / args.depth_scale, 
+												 os.path.join(log_dir, f'map_duster_rgb_{map_id}.png'), 
+												 os.path.join(log_dir, f'map_duster_depth_{map_id}.png'))
+
+			rgb_image = to_numpy(obs_node.rgb_image)
+			depth_image = to_numpy(obs_node.depth_image)
+			save_input_images(rgb_image * 255, depth_image / args.depth_scale,
+												os.path.join(log_dir, f'obs_gt_rgb_{obs_id}.png'), 
+ 											  os.path.join(log_dir, f'obs_gt_depth_{obs_id}.png'))
+			rgb_image = scene.imgs[1]
+			depth_image = to_numpy(scene.get_depthmaps())[1]
+			save_duster_images(rgb_image * 255, depth_image / args.depth_scale, 
+												 os.path.join(log_dir, f'obs_duster_rgb_{obs_id}.png'), 
+												 os.path.join(log_dir, f'obs_duster_depth_{obs_id}.png'))
+			######################################
+
 			im_poses = scene.get_im_poses()
 			im_poses = to_numpy(scene.get_im_poses())
 			est_T = im_poses[1]
-			# NOTE(gogojjh): change the definition of est_T since it is originally defined as T_obs_map
+			# Change the definition of est_T since it is originally defined as T_obs_map
 			if abs(np.sum(np.diag(est_T)) - 4.0) < 1e-5:
 				est_T = np.linalg.inv(im_poses[0])
 			print('Estimated Poses:\n', est_T)
@@ -139,5 +146,5 @@ def main(args):
 	save_error(np.array(rot_e), np.array(trans_e), log_dir)
 
 if __name__ == "__main__":
-		args = setup_args()
+		args = parse_arguments()
 		main(args)
