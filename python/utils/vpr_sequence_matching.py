f386f8a8fa302ab3c701626514936f0ed2353315
gogojjh <jiaojh1994@gmail.com>
2024-12-19 19:27:56 +0000

    bug: add augment edges but not fix the issue

--- python/utils/vpr_sequence_matching.py
+++ python/utils/vpr_sequence_matching.py
@@ -2,13 +2,17 @@
 
 import os
 import argparse
-import time
 import numpy as np
-from tqdm import tqdm
 from matplotlib import pyplot as plt
+from evo.core.trajectory import PosePath3D
+from utils_trajectory import align_trajectory
+import random
+import copy
 
 class PlaceRecognitionSeqMatching:
 	def __init__(self):
+		self.RANSAC_ITERATIONS = 1000
+
 		self.wContrast = 15
 		self.enhance = False  # False for learning-based VPR methods
 
@@ -59,7 +63,31 @@ class PlaceRecognitionSeqMatching:
 		# plt.close()
 		################################
 
-		return recall_preds, pred, score, db_ind_seq_match
+		return recall_preds, pred, score
+
+	def ransac_check_match(self, db_poses, query_poses, connected_indices):
+		best_min_rmse, best_indices, best_align_R_t_s = float('inf'), [], None
+		for _ in range(self.RANSAC_ITERATIONS):
+			sampled_indices = random.sample(connected_indices, max(1, len(connected_indices) // 10))
+			db_indices = [edge[0] for edge in sampled_indices]
+			query_indices = [edge[1] for edge in sampled_indices]
+			
+			traj_ref = PosePath3D(
+				positions_xyz=db_poses[db_indices, :3],
+				orientations_quat_wxyz=np.roll(db_poses[db_indices, 3:], -1)
+			)
+			traj_est = PosePath3D(
+				positions_xyz=query_poses[query_indices, :3],
+				orientations_quat_wxyz=np.roll(query_poses[query_indices, 3:], -1)
+			)
+			traj_ref, traj_est_aligned, ape_metric, align_R_t_s = align_trajectory(traj_ref, traj_est)
+			rmse = ape_metric.get_all_statistics()['rmse']
+			if rmse < best_min_rmse:
+				best_min_rmse = copy.deepcopy(rmse)
+				best_indices = copy.deepcopy(sampled_indices)
+				best_align_R_t_s = copy.deepcopy(align_R_t_s)
+		
+		return best_min_rmse, best_indices, align_R_t_s 
 
 	def _compute_diff_matrix(self, query_descriptors) -> np.ndarray:
 		"""
@@ -162,12 +190,14 @@ if __name__ == "__main__":
 	import pycpptools.src.python.utils_math as pytool_math
 	from tqdm import tqdm
 	from vpr_single_matching import PlaceRecognitionSingleMatching
+	import time
 
 	# Parse arguments
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--db_map_path", type=str, help="Path to the database map file")
 	parser.add_argument("--query_map_path", type=str, help="Path to the query map file")
 	args = parser.parse_args()
+
 	# Load database and query
 	db_map = GraphLoader.load_data(
 		args.db_map_path,
@@ -185,118 +215,98 @@ if __name__ == "__main__":
 		load_depth=False,
 		normalized=False
 	)
-	# Performance test
+
+	# Extract descriptors
 	db_descriptors = np.array([node.get_descriptor() for _, node in db_map.nodes.items()], dtype="float32")
 	db_poses = np.zeros((db_map.get_num_node(), 7), dtype="float32")
 	for indices, (_, node) in enumerate(db_map.nodes.items()):
-		db_poses[indices, :3] = node.trans
-		db_poses[indices, 3:] = node.quat
+		db_poses[indices, :3] = node.trans_gt
+		db_poses[indices, 3:] = node.quat_gt
 	query_descriptors = np.array([node.get_descriptor() for _, node in query_map.nodes.items()], dtype="float32")
 	query_poses = np.zeros((query_map.get_num_node(), 7), dtype="float32")
 	for indices, (_, node) in enumerate(query_map.nodes.items()):
-		query_poses[indices, :3] = node.trans
-		query_poses[indices, 3:] = node.quat
+		query_poses[indices, :3] = node.trans_gt
+		query_poses[indices, 3:] = node.quat_gt
+
 	# Create sequence matching model
 	model = PlaceRecognitionSeqMatching()
 	model.initialize_model(db_descriptors)
 	single_img_model = PlaceRecognitionSingleMatching()    
 	single_img_model.initialize_model(db_descriptors)
+
 	# Perform sequence matching
-	preds, scores = [], []
-	connected_edges = []
-	rmse_list = []
+	connected_indices = []
+	start_time = time.time()
 	for node in tqdm(query_map.nodes.values()):
-		# if node.id != 32: continue
 		if node.id - model.seqLen + 1 >= 0:
 			query_descs = query_descriptors[node.id-model.seqLen+1:node.id+1]
-			recall_preds, pred, score, db_ind_seq_match = model.match(query_descs, node.id, backward=False)
-
-			##### Check matching result by trajectory aignment
-			# from utils_trajectory import align_trajectory, plot_aligned_traj
-			# from evo.core.trajectory import PosePath3D
-			# traj_ref = PosePath3D(positions_xyz=db_poses[db_ind_seq_match, :3], 
-			# 					  orientations_quat_wxyz=np.roll(db_poses[db_ind_seq_match, 3:], 1))
-			# traj_est = PosePath3D(positions_xyz=query_poses[node.id-model.seqLen+1:node.id+1, :3], 
-			# 					  orientations_quat_wxyz=np.roll(query_poses[node.id-model.seqLen+1:node.id+1, 3:], 1))
-			# traj_ref, traj_est_aligned, ape_metric = align_trajectory(traj_ref, traj_est)
-			# ape_statistics = ape_metric.get_all_statistics()
-			# rmse_list.append(ape_statistics['rmse'])
-			# plot_aligned_traj(traj_ref, traj_est_aligned, ape_metric)
-			# exit()
-			##### 
-			preds.append(recall_preds)
-			scores.append(score)
-			connected_edges.append((db_map.get_node(pred), node))
+			recall_preds, pred, score = model.match(query_descs, node.id, backward=False)
+			connected_indices.append((pred, node.id, score))
 		else:
 			recall_preds, pred, score = single_img_model.match(node.get_descriptor().reshape(1, -1))
-			preds.append(recall_preds)
-			scores.append(score)
-			connected_edges.append((db_map.get_node(pred), node))
+			connected_indices.append((pred, node.id, score))
+	print(f"Sequence Matching Costs: {time.time() - start_time:.3f}s")
 
-	# Doing Ransac
-	# Create Trajectory
-	from evo.core.trajectory import PosePath3D
-	from utils_trajectory import align_trajectory, plot_aligned_traj
-	RANSAC_ITERATIONS = 100
-	min_error = float('inf')
-	best_edges = []
-	import random
-	for _ in range(RANSAC_ITERATIONS):
-		selected_edges = random.sample(connected_edges, max(1, len(connected_edges) // 10))
-		db_indices = [edge[0].id for edge in selected_edges]
-		query_indices = [edge[1].id for edge in selected_edges]
+	# RANSAC-based unreliable edges extraction
+	RMSE_THRESHOLD = 10.0
+	model.RANSAC_ITERATIONS = 100
+	for k in range(1):
+		best_min_rmse, best_indices, best_align_R_t_s = \
+			model.ransac_check_match(db_poses, query_poses, connected_indices)
+		print(best_align_R_t_s[0])
+		print(best_align_R_t_s[1])
+		print(best_align_R_t_s[2])
 
-		traj_ref = PosePath3D(positions_xyz=db_poses[db_indices, :3], 
-							  orientations_quat_wxyz=np.roll(db_poses[db_indices, 3:], 1))
-		traj_est = PosePath3D(positions_xyz=query_poses[query_indices, :3], 
-							  orientations_quat_wxyz=np.roll(query_poses[query_indices, 3:], 1))
-		traj_ref, traj_est_aligned, ape_metric = align_trajectory(traj_ref, traj_est)
-		ape_statistics = ape_metric.get_all_statistics()
-		if ape_statistics['rmse'] < min_error:
-			min_error = ape_statistics['rmse']
-			best_edges = selected_edges
+		print(f"RANSAC Runtime: {time.time() - start_time:.3f}s")
+		print(f"Best RMSE: {best_min_rmse:.3f}")
+		print(f"Candidates Size: {len(connected_indices)}")
+		print(f"Best Indices Size: {len(best_indices)}")
+		if best_min_rmse >= RMSE_THRESHOLD: 
+			continue
 
-	print(f"Best RMSE: {min_error}")
-	for edge in best_edges:
-		print(f"Edge: query {edge[1].id} - DB: {edge[0].id}")
+		# Augment edges for relative localization
+		edge_set = {f"{edge[0]}_{edge[1]}" for edge in best_indices}
+		# augment_indices = random.sample(connected_indices, max(1, len(connected_indices) // 5))
+		augment_indices = connected_indices
+		print(f"Aug Indices Size: {len(augment_indices)}")
+		for edge in augment_indices:
+			edge_id = f"{edge[0]}_{edge[1]}"
+			if edge_id not in edge_set:
+				db_node, query_node = db_map.get_node(edge[0]), query_map.get_node(edge[1])
+				error = np.linalg.norm(
+					best_align_R_t_s[0] @ query_node.trans.reshape(3, 1) +
+					best_align_R_t_s[1] - db_node.trans.reshape(3, 1)
+				)
+				print(error)
+				if error < RMSE_THRESHOLD:
+					best_indices.append(edge)
+					edge_set.add(edge_id)
+		print(f"After Aug Indices Size: {len(best_indices)}")
 
-	succ = 0
-	for edge in best_edges:
-		query_node = edge[0]
-		ref_map_node = edge[1]
-		dis_tsl, dis_angle = pytool_math.tools_eigen.compute_relative_dis(\
-			query_node.trans_gt, query_node.quat_gt, ref_map_node.trans_gt, ref_map_node.quat_gt)
-		if dis_tsl < 10.0:
-			succ += 1
-			print(f"Correct prediction: Query {query_node.id} - DB: {ref_map_node.id}")
-		else:
-			print(f"Wrong prediction: Query {query_node.id} - DB: {ref_map_node.id}")
-	print(f"Success Rate: {succ / len(best_edges)}")
-	################3
+		succ = 0
+		for edge in best_indices:
+			db_node, query_node = db_map.get_node(edge[0]), query_map.get_node(edge[1])
+			dis_tsl, dis_angle = pytool_math.tools_eigen.compute_relative_dis(\
+				query_node.trans_gt, query_node.quat_gt, db_node.trans_gt, db_node.quat_gt)
+			if dis_tsl < 10.0:
+				succ += 1
+				print(f"Correct prediction: Query {query_node.id} - DB: {db_node.id}")
+			else:
+				print(f"Wrong prediction: Query {query_node.id} - DB: {db_node.id}")
+		print(f"Success Rate: {succ / len(best_indices):.3f} {len(best_indices)}\n")
+		################
 
-	# succ = 0
-	# for i, node in enumerate(query_map.nodes.values()):
-	# 	ref_map_node = db_map.get_node(preds[i][0])
-	# 	dis_tsl, dis_angle = pytool_math.tools_eigen.compute_relative_dis(\
-	# 		node.trans_gt, node.quat_gt, ref_map_node.trans_gt, ref_map_node.quat_gt)
-	# 	if dis_tsl < 10.0:
-	# 		succ += 1
-	# 		print(f"Correct prediction: Query {node.id} - DB: {preds[i][0]} - {scores[i]:.3f} - {rmse_list[i]:.3f}")
-	# 	else:
-	# 		print(f"Wrong prediction: Query {node.id} - DB: {preds[i][0]} - {scores[i]:.3f} - {rmse_list[i]:.3f}")
-	# print(f"Success Rate: {succ / len(query_map.nodes)}")
-
-	fig, ax = plt.subplots(figsize=(10, 10))
-	for i, node in enumerate(query_map.nodes.values()):
-		ref_map_node = db_map.get_node(preds[i][0])
-		ax.plot(node.trans_gt[0], node.trans_gt[1], 'ko', markersize=5)
-		dis_tsl, dis_angle = pytool_math.tools_eigen.compute_relative_dis(\
-			node.trans_gt, node.quat_gt, ref_map_node.trans_gt, ref_map_node.quat_gt)
-		if dis_tsl < 10.0:
-			ax.plot(node.trans_gt[0], node.trans_gt[1], 'ro', markersize=5)
-	ax.grid(ls='--', color='0.7')
-	plt.axis('equal')
-	plt.xlabel('X-axis')
-	plt.ylabel('Y-axis')
-	plt.title(f"Success Rate: {succ / len(query_map.nodes)}")
-	plt.savefig("/Rocket_ssd/dataset/data_litevloc/map_multisession_eval/ucl_campus/s00000/out_map4/preds/result_place_recognition.png")
+		fig, ax = plt.subplots(figsize=(10, 10))
+		for edge in best_indices:
+			db_node, query_node = db_map.get_node(edge[0]), query_map.get_node(edge[1])
+			ax.plot(query_node.trans_gt[0], query_node.trans_gt[1], 'ko', markersize=5)
+			dis_tsl, dis_angle = pytool_math.tools_eigen.compute_relative_dis(\
+				query_node.trans_gt, query_node.quat_gt, db_node.trans_gt, db_node.quat_gt)
+			if dis_tsl < 10.0:
+				ax.plot(query_node.trans_gt[0], query_node.trans_gt[1], 'ro', markersize=5)
+		ax.grid(ls='--', color='0.7')
+		plt.axis('equal')
+		plt.xlabel('X-axis')
+		plt.ylabel('Y-axis')
+		plt.title(f"Success Rate: {succ / len(query_map.nodes)}")
+		plt.savefig(f"/Rocket_ssd/dataset/data_litevloc/map_multisession_eval/ucl_campus/s00000/out_map4/preds/result_PR_{k}.png")
