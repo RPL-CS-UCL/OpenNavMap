8e597d922e0bc8e328c3c47655bab9582478006c
gogojjh <jiaojh1994@gmail.com>
2025-03-21 12:28:14 +0000

    update test config for hkustgz_campus

--- scripts/run_benchmark_rpe_evaluation.sh
+++ scripts/run_benchmark_rpe_evaluation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if DATASET_NAME is provided
-if [ -z "$1" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
   echo "Error: DATASET_NAME is not specified."
   echo "Usage: ./run_benchmark_evaluation.sh <DATASET_NAME> (matterport3d, hkustgz_campus, ucl_campus, mapfree, hkust_aria)
   <SPLIT> (train, val, test)"
@@ -16,32 +16,37 @@ SPLIT=$2
 export PROJECT_PATH="/Titan/code/robohike_ws/src/litevloc"
 export DATASET_PATH="/Rocket_ssd/dataset/data_litevloc/map_free_eval"
 export CONFIG_FILE="$PROJECT_PATH/python/config/dataset/$DATASET_NAME.yaml"
-export N_QUERY=30 # same to submission
-export TOP_K=2
-# Optional: config_005_5, config_025_5, config_05_10, config_1_10, config_2_20
-export EVAL_CONFIGS=("config_025_5" "config_05_10")
+export N_QUERY=30
+export EVAL_CONFIGS=("config_05_10") # Optional: config_005_5, config_025_5, config_05_10, config_1_10, config_2_20
 
-# models=("master" "duster" "hloc_disk_dilg" "vpr_cosplace_resnet18_512")
 export MODELS=(
-  "master"
+	"hloc_disk_dilg"
+	"vpr_cosplace_resnet18_512"
+	"vpr_netvlad_resnet18_4096"
+  "master_nocalib_pretrain"
+  "master_calib_pretrain"
 	"duster_nocalib_pretrain"
-  "duster_nocalib_ftlora_pdepth"
-  "duster_nocalib_ftlora_gtdepth"
+  "duster_calib_pretrain"
 )
 
-for EVAL_CONFIG in "${EVAL_CONFIGS[@]}"
-do
-  for model in "${MODELS[@]}"
+for TOP_K in {2..10}; do
+  echo "Processing with TOP_K: $TOP_K"
+
+  for EVAL_CONFIG in "${EVAL_CONFIGS[@]}"
   do
-    echo "Evaluate pose_estimation methods: $model"   
-    python $PROJECT_PATH/python/benchmark_rpe/evaluation.py \
-      --config $CONFIG_FILE \
-      --submission_path $DATASET_PATH/$DATASET_NAME/map_free_eval/results_rpe/$model/submission_$TOP_K.zip \
-      --dataset_path $DATASET_PATH/$DATASET_NAME/map_free_eval \
-      --n_query $N_QUERY \
-      --split $SPLIT \
-      --eval_config $EVAL_CONFIG \
-      --log warning
-    echo ""
+    for model in "${MODELS[@]}"
+    do
+      echo "Evaluate pose_estimation methods: $model"   
+      python $PROJECT_PATH/python/benchmark_rpe/evaluation.py \
+        --config $CONFIG_FILE \
+        --submission_path $DATASET_PATH/$DATASET_NAME/map_free_eval/results_rpe/$model/submission_$TOP_K.zip \
+        --dataset_path $DATASET_PATH/$DATASET_NAME/map_free_eval \
+        --n_query $N_QUERY \
+        --split $SPLIT \
+        --eval_config $EVAL_CONFIG \
+        --log warning
+      echo ""
+    done
   done
+
 done
--- scripts/run_benchmark_rpe_submission.sh
+++ scripts/run_benchmark_rpe_submission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if DATASET_NAME is provided
-if [ -z "$1" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "Error: DATASET_NAME is not specified."
 	echo "Usage: ./run_benchmark_rpe_submission.sh <DATASET_NAME> (matterport3d, hkustgz_campus, ucl_campus, mapfree, hkust_aria) 
 	<SPLIT> (train, val, test)"
@@ -18,25 +18,32 @@ export DATASET_PATH="/Rocket_ssd/dataset/data_litevloc/map_free_eval"
 export CONFIG_FILE="$PROJECT_PATH/python/config/dataset/$DATASET_NAME.yaml"
 export OUT_DIR="$DATASET_PATH/$DATASET_NAME/map_free_eval/results_rpe"
 export N_QUERY=30
-export TOP_K=2
 
-# export MODELS="master hloc_disk_dilg vpr_cosplace_resnet18_512"
 export MODELS=(
-	"master"
+	"hloc_disk_dilg"
+	"vpr_cosplace_resnet18_512"
+	"vpr_netvlad_resnet18_4096"
+  "master_nocalib_pretrain"
+  "master_calib_pretrain"
 	"duster_nocalib_pretrain"
-	"duster_nocalib_ftlora_pdepth"
-	"duster_nocalib_ftlora_gtdepth"
+	"duster_calib_pretrain"
 )
 
 export LORA_PATHS=(
 	"none"
+	"none"
+	"none"
+	"none"
+	"none"
 	"none"	
-	"$DATASET_PATH/$DATASET_NAME/map_free_eval/finetune_duster_calib_pretrain/weights/duster_lora_pdepth.pt"
-	"$DATASET_PATH/$DATASET_NAME/map_free_eval/finetune_duster_calib_pretrain/weights/duster_lora_gtdepth.pt"
+	"none"
 )
 
-# Run the Python script
-for i in "${!MODELS[@]}"; do
+for TOP_K in {3..10}; do
+	echo "Processing with TOP_K: $TOP_K"
+	
+	# Run the Python script
+	for i in "${!MODELS[@]}"; do
 		MODEL="${MODELS[$i]}"
 		LORA_PATH="${LORA_PATHS[$i]}"
 		echo "Processing model: $MODEL with LoRA weight: $LORA_PATH"
@@ -44,16 +51,16 @@ for i in "${!MODELS[@]}"; do
 		python $PROJECT_PATH/python/benchmark_rpe/submission.py --config $CONFIG_FILE --models $MODEL \
 			--out_dir $OUT_DIR --n_query $N_QUERY --top_k $TOP_K \
 			--lora_path $LORA_PATH --split $SPLIT
-			#  --debug --viz
 		echo ""
-done			
+	done
 
-# Unzip files
-for MODEL in "${MODELS[@]}"; do
-		mkdir "$OUT_DIR/$MODEL/submission_$TOP_K"
+	# Unzip files
+	for MODEL in "${MODELS[@]}"; do
+		mkdir -p "$OUT_DIR/$MODEL/submission_$TOP_K"
 		if [ -f "$OUT_DIR/$MODEL/submission_$TOP_K.zip" ]; then
-				unzip -o "$OUT_DIR/$MODEL/submission_$TOP_K.zip" -d "$OUT_DIR/$MODEL/submission_$TOP_K"
+			unzip -o "$OUT_DIR/$MODEL/submission_$TOP_K.zip" -d "$OUT_DIR/$MODEL/submission_$TOP_K"
 		else
-				echo "Error: submission_$TOP_K.zip not found for model $MODEL"
+			echo "Error: submission_$TOP_K.zip not found for model $MODEL"
 		fi
+	done
 done
