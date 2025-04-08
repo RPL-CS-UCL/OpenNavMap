46f11527d789719dedd006e7df1dadbb25711aba
gogojjh <jiaojh1994@gmail.com>
2025-04-08 19:00:34 +0100

    use master_calib_pretrain

--- scripts/run_map_merging.sh
+++ scripts/run_map_merging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define path and number of submaps
-PathSubmap="/Rocket_ssd/dataset/data_litevloc/map_multisession_eval/ucl_campus_aria/s00001"
+PathSubmap="/Rocket_ssd/dataset/data_litevloc/map_multisession_eval/ucl_campus_aria/s00000"
 # NUM_SUBMAP=6
 
 In_Map=(
@@ -15,7 +15,15 @@ rosrun litevloc map_merge_pipeline.py \
   --input_submap_path "$PathSubmap/${In_Map[0]}" "$PathSubmap/${In_Map[1]}" \
   --output_map_path "$PathSubmap/$Out_Map" \
   --image_size 512 288 \
-  --vpr_match_model single_match # --viz
+  --vpr_match_model single_match \
+  --pose_estimation_method master_calib_pretrain \
+  --viz
+  # --select_keyframe
+
+# NOTE(gogojjh): Not used - Compute IQA
+# rosrun litevloc extract_iqa.py \
+#   --dataset_path /Rocket_ssd/dataset/data_litevloc/map_multisession_eval/ucl_campus_aria/s00000/out_map3 \
+#   --output /Rocket_ssd/dataset/data_litevloc/map_multisession_eval/ucl_campus_aria/s00000/out_map3
 
 # NOTE(gogojjh): Not used
 # rosrun litevloc pose3slam_g2o.py \
