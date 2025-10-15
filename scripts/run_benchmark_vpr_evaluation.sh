7aa0c63fb586ec18a094284afc1be02d7ee4c72f
gogojjh <jiaojh1994@gmail.com>
2025-10-15 10:10:59 +0100

    update readme

--- scripts/run_benchmark_vpr_evaluation.sh
+++ scripts/run_benchmark_vpr_evaluation.sh
@@ -11,32 +11,32 @@ DATASET_PATH=$1
 
 # Export environment variables
 export PROJECT_PATH="/Titan/code/robohike_ws/src/litevloc"
-METHODS="
-netvlad_VGG16_4096_single_match_1_none
-netvlad_VGG16_4096_single_match_1_master 
-netvlad_VGG16_4096_sequence_match_20_none 
-netvlad_VGG16_4096_sequence_match_20_master 
-netvlad_VGG16_4096_graph_search_1_none
-netvlad_VGG16_4096_graph_search_1_master
-cosplace_ResNet18_256_single_match_1_none 
-cosplace_ResNet18_256_single_match_1_master 
-cosplace_ResNet18_256_sequence_match_20_none 
-cosplace_ResNet18_256_sequence_match_20_master 
-cosplace_ResNet18_256_graph_search_1_none
-cosplace_ResNet18_256_graph_search_1_master
-eigenplaces_ResNet18_256_single_match_1_none 
-eigenplaces_ResNet18_256_single_match_1_master 
-eigenplaces_ResNet18_256_sequence_match_20_none 
-eigenplaces_ResNet18_256_sequence_match_20_master 
-eigenplaces_ResNet18_256_graph_search_1_none
-eigenplaces_ResNet18_256_graph_search_1_master
-anyloc-structured_DINOv2_49152_single_match_1_none 
-anyloc-structured_DINOv2_49152_single_match_1_master
-anyloc-structured_DINOv2_49152_sequence_match_20_none 
-anyloc-structured_DINOv2_49152_sequence_match_20_master 
-anyloc-structured_DINOv2_49152_graph_search_1_none
-anyloc-structured_DINOv2_49152_graph_search_1_master
-"
+# METHODS="
+# netvlad_VGG16_4096_single_match_1_none
+# netvlad_VGG16_4096_single_match_1_master 
+# netvlad_VGG16_4096_sequence_match_20_none 
+# netvlad_VGG16_4096_sequence_match_20_master 
+# netvlad_VGG16_4096_graph_search_1_none
+# netvlad_VGG16_4096_graph_search_1_master
+# cosplace_ResNet18_256_single_match_1_none 
+# cosplace_ResNet18_256_single_match_1_master 
+# cosplace_ResNet18_256_sequence_match_20_none 
+# cosplace_ResNet18_256_sequence_match_20_master 
+# cosplace_ResNet18_256_graph_search_1_none
+# cosplace_ResNet18_256_graph_search_1_master
+# eigenplaces_ResNet18_256_single_match_1_none 
+# eigenplaces_ResNet18_256_single_match_1_master 
+# eigenplaces_ResNet18_256_sequence_match_20_none 
+# eigenplaces_ResNet18_256_sequence_match_20_master 
+# eigenplaces_ResNet18_256_graph_search_1_none
+# eigenplaces_ResNet18_256_graph_search_1_master
+# anyloc-structured_DINOv2_49152_single_match_1_none 
+# anyloc-structured_DINOv2_49152_single_match_1_master
+# anyloc-structured_DINOv2_49152_sequence_match_20_none 
+# anyloc-structured_DINOv2_49152_sequence_match_20_master 
+# anyloc-structured_DINOv2_49152_graph_search_1_none
+# anyloc-structured_DINOv2_49152_graph_search_1_master
+# "
 
 # METHODS="
 # cosplace_ResNet18_256_single_match_1_none 
@@ -51,15 +51,23 @@ anyloc-structured_DINOv2_49152_graph_search_1_master
 # cosplace_ResNet18_256_sequence_match_20_none cosplace_ResNet18_256_sequence_match_50_none cosplace_ResNet18_256_graph_search_1_none 
 # "
 
+METHODS="
+megaloc_Dinov2_8448_single_match_1_none 
+megaloc_Dinov2_8448_single_match_1_master 
+megaloc_Dinov2_8448_sequence_match_20_none 
+megaloc_Dinov2_8448_sequence_match_20_master 
+"
+
 # Evaluation and generate report_evaluation.txt
 # ucl_campus - trans_threshold: 7.5, ori_threshold: 75.0
 # robocar - trans_threshold: 24.0, ori_threshold: 75.0
+# wildscenes - trans_threshold: 7.5, ori_threshold: 75.0
 echo "Evaluate VPR methods: "
 python $PROJECT_PATH/python/benchmark_vpr/evaluation.py \
   --result_dir $DATASET_PATH/results_vpr \
   --methods $METHODS \
   --dataset_path $DATASET_PATH \
-  --trans_threshold 24.0 \
+  --trans_threshold 7.5 \
   --ori_threshold 75.0 \
   --log warning \
   --option eval
